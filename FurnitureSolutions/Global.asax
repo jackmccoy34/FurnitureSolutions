c0ffde5ca95b99ea1ac54d360939c701d42c4f9d
Jack McCoy <jackmccoy34@gmail.com>
2020-09-11 14:34:09 -0400

    Add project files.

---
+++ FurnitureSolutions/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FurnitureSolutions.MvcApplication" Language="C#" %>
---
+++ FurnitureSolutions/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FurnitureSolutions.Startup))]
+namespace FurnitureSolutions
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
