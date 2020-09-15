f4219c3ff2043c9c8aaeb9dc8555e1c026bdabfc
Jack McCoy <jackmccoy34@gmail.com>
2020-09-15 12:49:39 -0400

    Product Index

---
+++ FurnitureSolutions.Models/ProductListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureSolutions.Models
+{
+    public class ProductListItem
+    {
+        public int ProductId { get; set; }
+        public string FurnitureType { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ FurnitureSolutions/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using FurnitureSolutions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FurnitureSolutions.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            var model = new ProductListItem[0];
+            return View(model);
+        }
+    }
+}
